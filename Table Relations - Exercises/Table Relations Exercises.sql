8bbfa50c64ad1427ff2e6a66f47f04f96318c797
Runador <runador123456@gmail.com>
2024-10-06 23:02:10 +0300

    Adding Table Relations Exercises.sql

--- Table Relations - Exercises/Table Relations Exercises.sql
+++ Table Relations - Exercises/Table Relations Exercises.sql
@@ -3,20 +3,20 @@ CREATE DATABASE people;
 USE people;
 DROP DATABASE people;
 
+CREATE TABLE passports (
+	passport_id INT PRIMARY KEY AUTO_INCREMENT,
+    passport_number VARCHAR(8) UNIQUE
+);
+
 CREATE TABLE people (
 	person_id INT PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(100) NOT NULL,
-    salary DOUBLE(9,2),
-    passport_id INT,
+    first_name VARCHAR(100),
+    salary DECIMAL(7,2),
+    passport_id INT UNIQUE,
     
     CONSTRAINT fk_people_passports
     FOREIGN KEY (passport_id)
     REFERENCES passports (passport_id)
-);
-
-CREATE TABLE passports (
-	passport_id INT PRIMARY KEY AUTO_INCREMENT,
-    passport_number VARCHAR(8)
 );
 
 INSERT INTO passports VALUES
@@ -25,9 +25,9 @@ INSERT INTO passports VALUES
     (103, 'ZE657QP2');
 
 INSERT INTO people (first_name, salary, passport_id) VALUES
-	('Roberto', 43300.00, 102),
-    ('Tom', 56100.00, 103),
-    ('Yana', 60200.00, 101);
+	('Roberto', 43300, 102),
+    ('Tom', 56100, 103),
+    ('Yana', 60200, 101);
     
 -- 2
 CREATE DATABASE cars;
@@ -63,16 +63,167 @@ INSERT INTO models VALUES
     (106, 'Nova', 3);
     
 -- 3
+CREATE SCHEMA students;
+USE students;
 
+CREATE TABLE students (
+	student_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE exams (
+	exam_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE students_exams (
+	student_id INT,
+    exam_id INT,
+    
+    PRIMARY KEY (student_id, exam_id),
+    FOREIGN KEY (student_id) REFERENCES students(student_id),
+    FOREIGN KEY (exam_id) REFERENCES exams(exam_id)
+);
+
+INSERT INTO students (name) VALUES 
+	('Mila'),
+    ('Toni'),
+    ('Ron');
+    
+INSERT INTO exams VALUES 
+	(101, 'Spring MVC'),
+    (102, 'Neo4j'),
+    (103, 'Oracle 11g');
+    
+INSERT INTO students_exams VALUES 
+	(1, 101),
+    (1, 102),
+    (2, 101),
+    (3, 103),
+    (2, 102),
+    (2, 103);
+    
 -- 4
+CREATE TABLE teachers (
+	teacher_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    manager_id INT
+);
+
+INSERT INTO teachers VALUES
+	(101, 'John', NULL),
+    (102, 'Maya', 106),
+    (103, 'Silvia', 106),
+    (104, 'Ted', 105),
+    (105, 'Mark', 101),
+    (106, 'Greta', 101);
+    
+ALTER TABLE teachers
+ADD FOREIGN KEY (manager_id) 
+REFERENCES teachers(teacher_id);
 
 -- 5
+CREATE DATABASE online_store;
+USE online_store;
+
+CREATE TABLE item_types (
+	item_type_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50)
+);
+
+CREATE TABLE items (
+	item_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50),
+    item_type_id INT,
+    
+    FOREIGN KEY (item_type_id)
+    REFERENCES item_types (item_type_id)
+);
+
+CREATE TABLE cities (
+	city_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50)
+);
+
+CREATE TABLE customers (
+	customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    birthday DATE,
+    city_id INT,
+    
+    FOREIGN KEY (city_id)
+    REFERENCES cities (city_id)
+);
+
+CREATE TABLE orders (
+	order_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT,
+    
+    FOREIGN KEY (customer_id)
+    REFERENCES customers (customer_id)
+);
+
+CREATE TABLE order_items (
+	order_id INT,
+    item_id INT,
+    
+    PRIMARY KEY (order_id, item_id),
+    FOREIGN KEY (order_id) REFERENCES orders (order_id),
+    FOREIGN KEY (item_id) REFERENCES items (item_id)
+);
 
 -- 6
+CREATE DATABASE university;
+USE university;
+
+CREATE TABLE subjects (
+	subject_id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    subject_name VARCHAR(50)
+);
+
+CREATE TABLE majors (
+	major_id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50)
+);
+
+CREATE TABLE students (
+	student_id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    student_number VARCHAR(12),
+    student_name VARCHAR(50),
+    major_id INT(11),
+    
+    FOREIGN KEY (major_id) REFERENCES majors (major_id)
+);
+
+CREATE TABLE payments (
+	payment_id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    payment_date DATE,
+    payment_amount DECIMAL(8, 2),
+    student_id INT(11),
+    
+    FOREIGN KEY (student_id) REFERENCES students(student_id)
+);
+
+CREATE TABLE agenda (
+	student_id INT(11),
+    subject_id INT(11),
+    
+    PRIMARY KEY (student_id, subject_id),
+    FOREIGN KEY (student_id) REFERENCES students(student_id),
+    FOREIGN KEY (subject_id) REFERENCES subjects(subject_id)
+);
 
 -- 7
+-- Направено.
 
 -- 8
 -- Направено.
 
 -- 9
+SELECT m.mountain_range, p.peak_name, p.elevation AS peak_elevation
+FROM mountains m
+	JOIN peaks p ON
+    m.id = p.mountain_id
+WHERE m.mountain_range = 'Rila'
+ORDER BY peak_elevation DESC;
+
